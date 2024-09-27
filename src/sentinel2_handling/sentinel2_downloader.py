29ace938dc913109bf7d241efe2ac99d86e8aa4a
colintzw <colintzw@gmail.com>
2024-09-27 11:43:05 +0800

    parallelize downloads and update gitignore

--- src/sentinel2_handling/sentinel2_downloader.py
+++ src/sentinel2_handling/sentinel2_downloader.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 
 import planetary_computer
 import pystac_client
+from joblib import Parallel, delayed
 
 from src.sentinel2_handling.stac_item_sentinel2_processor import (
     StacItemSentinel2Processor,
@@ -37,23 +38,36 @@ def query_sentinel2(bbox, max_cloud_cover=80, end_date=None, num_days_before_end
 
 
 def filter_item_list(
-    item_list, bbox, min_usable_pct=85
+    item_list, bbox, min_usable_pct=85, njobs=-1
 ) -> Tuple[List[str], List[StacItemSentinel2Processor]]:
     good_item_metadata = []
     good_item_processors = []
-    for item in item_list:
-        item_date = item.properties["datetime"][:10]
-        prev_cloud_cover = item.properties["eo:cloud_cover"]
-        item_proc = StacItemSentinel2Processor(item=item, bbox=bbox)
-        usable_pixel_percentage = item_proc.compute_usable_pixels()
-        if usable_pixel_percentage >= min_usable_pct:
-            good_item_metadata.append(
-                (item_date, prev_cloud_cover, usable_pixel_percentage)
-            )
-            good_item_processors.append(item_proc)
+
+    results = Parallel(n_jobs=njobs)(
+        delayed(get_processor_and_metadata)(
+            item=item, bbox=bbox, min_usable_pct=min_usable_pct
+        )
+        for item in item_list
+    )
+
+    for res in results:
+        if res[0] is None:
+            continue
+        good_item_metadata.append(res[0])
+        good_item_processors.append(res[1])
 
     print(
         f"List filtered as {len(good_item_metadata)} out of {len(item_list)} items orginally"
     )
     return (good_item_metadata, good_item_processors)
 
+
+def get_processor_and_metadata(item, bbox, min_usable_pct):
+    item_date = item.properties["datetime"][:10]
+    prev_cloud_cover = item.properties["eo:cloud_cover"]
+    item_proc = StacItemSentinel2Processor(item=item, bbox=bbox)
+    usable_pixel_percentage = item_proc.compute_usable_pixels()
+    if usable_pixel_percentage < min_usable_pct:
+        return (None, None)
+    metadata = (item_date, prev_cloud_cover, usable_pixel_percentage)
+    return (metadata, item_proc)
